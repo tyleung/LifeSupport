7c71f99324ba0389174912f1a760ea33d1c4ae8c
tyleung <tyleung@ualberta.ca>
2014-08-13 22:42:43 -0600

    Added required appcompat project

---
+++ LifeSupport/src/ca/eccc/lifesupport/ftpclient/FtpClient.java
@@ -0,0 +1,17 @@
+package ca.eccc.lifesupport.ftpclient;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+public class FtpClient {
+
+	private FTPClient ftpClient = new FTPClient();
+	
+	public FTPClient getFtpClient() {
+		return this.ftpClient;
+	}
+	
+	public void connect() {
+	}
+	
+	
+}
